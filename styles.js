84c55852ff19282b9369eb46405b8e803f0c0999
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-11 20:18:21 -0500

    The Html Elements look good. Need to get functionality working.

---
+++ styles.js
@@ -0,0 +1,15 @@
+//will need to create the hours in a day... either coded in html or dynamic in JS
+//will need to be able to update hours on given time elements
+//need to be able to create writable area/form
+//need a btn to save userInput to the inspector
+
+var = hours in a day
+var = btn
+
+
+var = layoutWriter = function (){
+for (var i=0; i=24; i++) {
+
+}
+
+}
